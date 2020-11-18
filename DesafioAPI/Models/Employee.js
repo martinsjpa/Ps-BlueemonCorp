cd487f0f64af18590aa17597f1423d486df538e1
Matheus Martins <matheusbarbosajpa3@gmail.com>
2020-11-18 02:49:10 -0300

    Crud Finalizado no Back-END

--- DesafioAPI/Models/Employee.js
+++ DesafioAPI/Models/Employee.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const ObjectId = Schema.ObjectId;
 const Employee = new mongoose.Schema({
+    id:ObjectId,
     nome:String,
     cpf:String,
     email:String,
--- DesafioAPI/app.js
+++ DesafioAPI/app.js
@@ -1,12 +1,31 @@
 
+/* const para utilizar a dependecia express */
 const express = require('express');
+
+/* const para utilizar a dependecia mongoose */
 const mongoose = require('mongoose');
+
+/* const para utilizar a dependecia cors */
+const cors = require('cors');
+
+/* Inicializa o Express */
 const app = express();
 
+/* Faz a Requição do Modelo Employee para ser utilizado no mongoDB  */
 require("./Models/Employee");
 
+/* Permite que o app utilize o formato JSON */
 app.use(express.json());
 
+/* Define quem pode solicitar chamadas para api */
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "http://http//localhost:4200");
+    res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
+    app.use(cors());
+    next();
+});
+
+/* Const para Acessar a Tabela Employee do Banco de dados  */
 const Employee = mongoose.model('employee');
 
 mongoose.connect('mongodb://127.0.0.1/cadastro', {
@@ -22,9 +41,7 @@ mongoose.connect('mongodb://127.0.0.1/cadastro', {
     console.log(erro);
 });
 
-app.get("/", (req, res) =>{
-    res.send("introdução");
-});
+
 
 
 
@@ -32,7 +49,61 @@ app.listen(8080, ()=> {
     console.log('servidor iniciado na porta 8080');
 })
 
-app.post("/employee", (req,res) => {
-    console.log(req.body);
-    return res.json({"mec":"tref"});
+app.get("/getEmployee", (req, res) =>{
+    Employee.find({}).then((employee) => {
+        return res.json(employee);
+    }).catch((erro) => {
+        return res.status(400).json({
+            error: true,
+            message: "Nenhum artigo encontrado!"
+        })
+    })
+});
+
+app.post("/saveEmployee", (req,res) => {
+    const employee = Employee.create(req.body, (error) => {
+        if(error)
+         {
+             return res.status(400).json({
+                 error:true,
+                 message:"error:employee não cadastrado"
+             })
+         }
+         return res.status(200).json({
+            error:false,
+            message:"Sucess: Employee Cadastrado"
+        }) 
+    });
+})
+
+app.put("/editEmployee/:id", (req,res) => {
+    const employee = Employee.updateOne({ _id: req.params.id}, req.body, (error) => {
+        if(error)
+        {
+            return res.status(400).json({
+                error:true,
+                message:"Error: não foi possível editar o employee"
+            });
+        }
+        return res.status(400).json({
+            error:false,
+            message:"Employee editado com sucesso"
+        });
+    })
+})
+
+app.delete("/deleteEmployee/:id", (req,res) => {
+    const employee = Employee.deleteOne({ _id: req.params.id}, req.body, (error) => {
+        if(error)
+        {
+            return res.status(400).json({
+                error:true,
+                message:"Error: não foi possivel excluir o employee"
+            });
+        }
+        return res.status(400).json({
+            error:false,
+            message:"Employee Excluido com sucesso"
+        });
+    })
 })
